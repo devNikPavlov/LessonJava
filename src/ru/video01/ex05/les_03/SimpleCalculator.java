82bc67bc2978ef0ec510bebc435564b095b3765f
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-22 20:38:14 +0300

    Book01. ex05. les_03

---
+++ src/ru/video01/ex05/les_03/SimpleCalculator.java
@@ -0,0 +1,26 @@
+package ru.video01.ex05.les_03;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        System.out.println("Введите значение a: ");
+        var a = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите значение b: ");
+        var b = new Scanner(System.in).nextInt();
+
+        var aPlusB = a + b;
+        var aMinusB = a - b;
+        var aMulB = a * b;
+        var aDivB = a / b;
+        var aModB = a % b;
+
+        System.out.println("a + b = " + aPlusB);
+        System.out.println("a - b = " + aMinusB);
+        System.out.println("a * b = " + aMulB);
+        System.out.println("a / b = " + aDivB);
+        System.out.println("a % b = " + aModB);
+    }
+}
