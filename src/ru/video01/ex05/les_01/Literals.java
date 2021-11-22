540cc0ca0babddb4e7ecabfd6b1aeab2bd786936
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-22 12:38:29 +0300

    Book01. ex05. les_01

---
+++ src/ru/video01/ex05/les_01/Literals.java
@@ -0,0 +1,25 @@
+package ru.video01.ex05.les_01;
+
+public class Literals {
+    public static void main(String[] args) {
+
+        // int
+        var i = 1;
+
+        // double
+        var d1 = 1.2;
+
+        // boolean
+        var b = true;
+
+        // char
+        var ch = 'a';
+
+        // string
+        var a = "nik";
+
+        System.out.printf(a);
+
+        System.out.println(a);
+    }
+}
