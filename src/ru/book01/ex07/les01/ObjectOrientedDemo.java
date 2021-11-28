f31b4e9515b483ff765b12cebb3864cc5b02bfb2
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-28 18:38:32 +0300

    book01. ex07. les01 Начало создания Классов

---
+++ src/ru/book01/ex07/les01/ObjectOrientedDemo.java
@@ -0,0 +1,14 @@
+package ru.book01.ex07.les01;
+
+/**
+ * Синтаксис объявления класса
+ * Модификатор доступа class ClassName {
+ *     // Тут - Содержимое класса с полями, конструкторами и методами
+ * }
+ */
+
+public class ObjectOrientedDemo {
+    public static void main(String[] args) {
+
+    }
+}
