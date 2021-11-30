211b07163d88abc168e5e9063854ab059694071d
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-11-30 17:08:56 +0300

    Video01. ex06. les08

---
+++ src/ru/video01/ex06/les_08/DisplayMaxNumber.java
@@ -0,0 +1,13 @@
+package ru.video01.ex06.les_08;
+
+public class DisplayMaxNumber {
+    public static void main(String[] args) {
+
+        var a = 5;
+        var b = 6;
+
+        var result = a > b ? a : b;
+
+        System.out.println(result);
+    }
+}
