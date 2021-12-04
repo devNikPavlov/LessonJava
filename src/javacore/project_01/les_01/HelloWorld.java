40b9526c6d3b0d8910c703dd79034799d462d2b7
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-04 18:34:56 +0300

    JavaCore. Project_01. Lesson_01

---
+++ src/javacore/project_01/les_01/HelloWorld.java
@@ -0,0 +1,14 @@
+package javacore.project_01.les_01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+main - функция
+
+
+ */
