a8766cf6ef0e906bbfeb41235b173df767eab1d0
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-04 18:54:51 +0300

    JavaCore. Project_03. Lesson_01

---
+++ src/javacore/project_03/les_01/Example.java
@@ -0,0 +1,30 @@
+package javacore.project_03.les_01;
+
+public class Example {
+    public static void main(String[] args) {
+
+        int value = 25;
+        System.out.println(value);
+
+        double value2 = 78.32;
+        System.out.println(value2);
+
+        boolean value3 = true;
+        System.out.println(value3);
+
+        char value4 = 'a';
+        System.out.println(value4);
+
+        value4 = 'b';
+        System.out.println(value4);
+
+        final char value5 = 'a';
+        System.out.println(value5);
+
+        int temp;
+        temp = 123;
+        System.out.println(temp);
+
+
+    }
+}
