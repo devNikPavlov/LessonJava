bf2b9f98aa3c9d1ce024d669d6c0cacd23d2ba06
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-06 22:14:28 +0300

    JavaCore. Project_04. Lesson_01

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Project_01 - написание первой программы Hello World
 Project_02 - примитивные типы данных
 Project_03 - переменные
+Project_04 - операторы if - else
---
+++ src/javacore/project_04/les_01/Example.java
@@ -0,0 +1,16 @@
+package javacore.project_04.les_01;
+
+public class Example {
+    public static void main(String[] args) {
+
+        int value = 10;
+
+        if (value > 0) {
+            System.out.println("Число является положительным");
+        } else  if (value < 0) {
+            System.out.println("Число является отрицательным");
+        } else {
+            System.out.println("Число является 0");
+        }
+    }
+}
