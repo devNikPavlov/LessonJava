7095f5d06832f07bc1d1f12a8b01a11d10b6111c
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-07 19:02:54 +0300

    JavaCore. Project_08. Lesson_01

---
+++ src/javacore/project_08/les_01/Task01.java
@@ -0,0 +1,10 @@
+package javacore.project_08.les_01;
+
+public class Task01 {
+    public static void main(String[] args) {
+
+        for (int value = 10; value > 0; value--) {
+            System.out.println(value);
+        }
+    }
+}
