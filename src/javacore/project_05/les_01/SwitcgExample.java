2e6d54e66eee6f6130b038fbb4c39ea59f319d40
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-07 17:18:00 +0300

    JavaCore. Project_05. Lesson_01

---
+++ src/javacore/project_05/les_01/SwitcgExample.java
@@ -0,0 +1,29 @@
+package javacore.project_05.les_01;
+
+public class SwitcgExample {
+    public static void main(String[] args) {
+
+        int value =2;
+
+        switch (value) {
+            case 1:
+                System.out.println("value == 1");
+                break;
+
+            case 2:
+                System.out.println("value == 2");
+                break;
+
+            case 3:
+                System.out.println("value == 3");
+                break;
+
+            case 4:
+                System.out.println("value == 4");
+                break;
+
+            default:
+                System.out.println("Нет такого значения");
+        }
+    }
+}
