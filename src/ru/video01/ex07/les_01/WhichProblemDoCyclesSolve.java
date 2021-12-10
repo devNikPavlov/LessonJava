d2be5df77716218f08bbf72c0e3324b04032fecc
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-10 17:28:32 +0300

    Vidoe_01. exp07. les_01

---
+++ src/ru/video01/ex07/les_01/WhichProblemDoCyclesSolve.java
@@ -0,0 +1,12 @@
+package ru.video01.ex07.les_01;
+
+// Операция цикла for
+
+public class WhichProblemDoCyclesSolve {
+    public static void main(String[] args) {
+
+        for (int i=1; i <=5; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
