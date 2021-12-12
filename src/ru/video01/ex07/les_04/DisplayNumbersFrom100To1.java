b1c268856f3c64537d919bed66e62b8a3f875964
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-12 11:14:45 +0300

    Vidoe_01. exp07. les_04

---
+++ src/ru/video01/ex07/les_04/DisplayNumbersFrom100To1.java
@@ -0,0 +1,10 @@
+package ru.video01.ex07.les_04;
+
+public class DisplayNumbersFrom100To1 {
+    public static void main(String[] args) {
+
+        for (var i=100; i>0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
