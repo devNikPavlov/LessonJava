5bd60d748fcad06c2f6999574a0f050083cf6e7f
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-12 11:21:57 +0300

    Vidoe_01. exp07. les_04

--- src/ru/video01/ex07/les_04/DisplayNumbersFrom100To1.java
+++ src/ru/video01/ex07/les_04/DisplayNumbersFrom100To1.java
@@ -3,8 +3,20 @@ package ru.video01.ex07.les_04;
 public class DisplayNumbersFrom100To1 {
     public static void main(String[] args) {
 
-        for (var i=100; i>0; i--) {
+        //мое решение
+//        for (var i=100; i>=1; i--) {
+//            System.out.print(i + " ");
+//        }
+
+        // можно еще и так
+
+        var from = 100;
+        var to = 1;
+        var step = 1;
+
+        for (int i = from; i >= to; i -= step) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
 }
