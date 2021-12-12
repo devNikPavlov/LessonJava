e4a9f042b0db04e24d787527c96252d3f35ea713
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2021-12-12 13:29:11 +0300

    Vidoe_01. exp07. les_09

---
+++ src/ru/video01/ex07/les_09/DisplayEvenNumbersFrom0To50.java
@@ -0,0 +1,12 @@
+package ru.video01.ex07.les_09;
+
+public class DisplayEvenNumbersFrom0To50 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 50) {
+            System.out.print(i++ + " ");
+        }
+        System.out.println();
+    }
+}
