5854f096307c4c4fb7991780da488760b87b957e
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-11 13:06:58 +0300

    Book02, exp06, lesson 06

---
+++ src/ru/book02/exp06/lesson06/Box.java
@@ -0,0 +1,12 @@
+package ru.book02.exp06.lesson06;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    // рассчитать и возвратить объем
+    double volume() {
+        return width * height * depth;
+    }
+}
