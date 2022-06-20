43018a5e0bdeaea6ea805db429318c087c5971c9
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-20 10:55:28 +0300

    Урок 1. Примитивные типы данных

---
+++ src/javaproject/lesson_01/Main.java
@@ -0,0 +1,30 @@
+package javaproject.lesson_01;
+
+//TODO Примитивные типы данных
+
+public class Main {
+    public static void main(String[] args) {
+
+        int temp;
+        temp = 567;
+        System.out.println(temp);
+
+        int value = 26;
+        System.out.println(value);
+
+        double value2 = 26.77;
+        System.out.println(value2);
+
+        boolean value3 = true;
+        System.out.println(value3);
+
+        char i = 'i';
+        System.out.println(i);
+
+        // константа, нельзя изменить
+        final int v = 25;
+        //v = 26;
+
+    }
+
+}
