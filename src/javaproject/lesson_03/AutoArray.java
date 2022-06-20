9c59704dd68e025256e891466e934aacea5140d6
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-20 12:10:52 +0300

    Урок 3. Пример 2

---
+++ src/javaproject/lesson_03/AutoArray.java
@@ -0,0 +1,13 @@
+package javaproject.lesson_03;
+
+//TODO Пример, Масиивы
+
+// усовершенствованная версия предыдущей программы
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int mohtDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.println("В апреле " + mohtDays[3] + " дней.");
+    }
+}
