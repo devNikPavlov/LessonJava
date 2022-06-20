0cc31657ca36155c25783900da2033c3df1fd9f6
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-20 11:48:12 +0300

    Урок 3. Массивы

---
+++ src/javaproject/lesson_03/Main.java
@@ -0,0 +1,14 @@
+package javaproject.lesson_03;
+
+//TODO Массивы
+
+public class Main {
+    public static void main(String[] args) {
+
+        // пример резервирования пямити для 12-элементов массива целых значений, при этом все элементы данного
+        // массива будут инициализированы нулевым значением
+
+        int monthDays[];
+        monthDays = new int[12];
+    }
+}
