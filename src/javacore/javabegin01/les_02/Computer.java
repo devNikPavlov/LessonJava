bd2c20eb51eef48db83981b54d04163ae0c8170e
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-06-29 18:29:56 +0300

    javabegin 1. Пример 2

---
+++ src/javacore/javabegin01/les_02/Computer.java
@@ -0,0 +1,14 @@
+package javacore.javabegin01.les_02;
+
+public class Computer {
+
+    String name = "IBM";
+
+    void on() {
+        System.out.println(name + " on");
+    }
+
+    void off() {
+        System.out.println(name + " off");
+    }
+}
