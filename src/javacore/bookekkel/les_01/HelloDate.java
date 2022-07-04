abe0d65b194cc0c6d08ee816c4c40a61d1bbbc13
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-07-04 17:53:01 +0300

    bookekkel, lesson 01

---
+++ src/javacore/bookekkel/les_01/HelloDate.java
@@ -0,0 +1,12 @@
+package javacore.bookekkel.les_01;
+
+// выводит на экран строку, а затем текущую дату
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
