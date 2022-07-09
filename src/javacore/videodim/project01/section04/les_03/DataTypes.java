d60591e97e2da7b43577985dc0117a57e2071b86
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-07-09 12:32:04 +0300

    Dim, pro01, sec04, les03

---
+++ src/javacore/videodim/project01/section04/les_03/DataTypes.java
@@ -0,0 +1,23 @@
+package javacore.videodim.project01.section04.les_03;
+
+//TODO Основные типы данных
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // целые
+        int i = 12;
+        System.out.println(Integer.MIN_VALUE + " < " + i + " < " + Integer.MAX_VALUE);
+
+        // плавающая точка
+        double w = 1.2;
+        System.out.println(Double.MIN_VALUE + " < " + w + " < " + Double.MAX_VALUE);
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char ch = 'a';
+
+        String s = "Hello";
+    }
+}
