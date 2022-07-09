65bb725544e823dfbc37249ad3fa1edae3cfbd9f
Nikolay Pavlov <devnikolaypavlov@gmail.com>
2022-07-09 12:04:33 +0300

    Dim, pro01, sec04, les01

---
+++ src/javacore/videodim/project01/section04/les_01/Out.java
@@ -0,0 +1,19 @@
+package javacore.videodim.project01.section04.les_01;
+
+//TODO Вывод результата на консоль
+
+public class Out {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+        System.out.println(1 + 4);
+
+        System.out.println();
+
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+    }
+
+}
